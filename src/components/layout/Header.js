cbde3b780a3abafb3f2408e368874e35be02ac91
Ali FateFul <ali.fateful21@gmail.com>
2024-04-27 21:55:50 +0330

    Header updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,19 +2,20 @@ import { AppBar, Container, Toolbar, Typography } from "@mui/material";
 import BookIcon from "@mui/icons-material/Book";
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
 	return (
 		<div>
 			<AppBar position='sticky'>
-				<Container max-width="lg">
+				<Container max-width='lg'>
 					<Toolbar>
 						<Typography
 							component='h1'
 							variant='h5'
 							fontWeight='700'
 							flex={1}>
-							وبلاگ FateFul
+							<Link to='/' style={{textDecoration: "none" , color :"white"}}>وبلاگ FateFul</Link>
 						</Typography>
 						<BookIcon />
 					</Toolbar>
